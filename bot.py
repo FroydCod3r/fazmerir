0df278451623f6c08767f35897c166f25d232a33
FroydCod3r <FroydCod3r@users.noreply.github.com>
2021-09-23 23:31:39 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+import telebot
+from datetime import datetime
+
+APIKEY = "2012484895:AAHmYqe2cQD4kDnKssdumjsgRJ6UcBpc9G0"
+
+bot = telebot.TeleBot(APIKEY)
+
+def diaDoSalario():
+    now = datetime.now()
+    dia_atual = now.day
+    diaRestante = 30-dia_atual
+    return diaRestante
+
+def verify(mensagem):
+    if mensagem.text == '/salario':
+        return True
+    else:
+        return False
+    # return True
+
+#decorator
+#@bot.message_handler(commands=["salario"])
+@bot.message_handler(func=verify)
+def responder(mensagem):
+    bot.reply_to(mensagem, "Falta apenas {} dias para o FAZ ME RIR".format(diaDoSalario()))
+
+bot.polling()
